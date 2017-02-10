164dd14d5c4c1adcd8a9e6eac483302ba6d17fff
thonier <thonier30@gmail.com>
2017-02-10 05:30:21 +0000

    First Users Test

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
+  get 'static_pages/hulk'
+
+  resources :microposts
+  resources :users
+  root 'users#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
